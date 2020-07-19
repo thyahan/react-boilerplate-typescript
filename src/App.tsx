da689ea14d9d0d62c755f6c81603f516d3d811e4
Yutthana Yahan <thyahan@gmail.com>
2020-07-19 17:01:34 +0700

    Add prettier Add eslint Add Antd with custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-     hello eieie
-    </div>
-  );
-}
+const App = () => {
+  return <div>Hello App here</div>;
+};
 
 export default App;
