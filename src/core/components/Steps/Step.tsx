0fc254418cc585a8dd8a2ee093ec2ab848126f5f
Yutthana Yahan <thyahan@gmail.com>
2020-07-26 01:45:05 +0700

    Update Steps component

--- src/core/components/Steps/Step.tsx
+++ src/core/components/Steps/Step.tsx
@@ -3,27 +3,53 @@ import styled from "styled-components";
 
 const selectedColor = "rgb(116, 92, 185)";
 const unSelectedColor = "rgb(199, 189, 226)";
-const rootBackgroundColor = "rgb(240, 242, 245)";
+const defaultParentBGColor = "rgb(240, 242, 245)";
 
-const Styled = styled.div<{ selected: boolean | undefined }>`
-  position: relative;
+interface IStyled {
+  selected: boolean | undefined;
+  parentBackground?: string | undefined;
+}
+
+const Styled = styled.div<IStyled>`
   width: 100%;
   height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+  margin: 0 10px 0 0;
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+
+  :before {
+    content: "";
+    position: absolute;
+    left: 0px;
+    border-style: solid;
+    border-width: 30px 0 30px 20px;
+    border-color: transparent transparent transparent
+      ${({ parentBackground }) => parentBackground || defaultParentBGColor};
+    z-index: 0;
+  }
+
+  :after {
+    content: "";
+    position: absolute;
+    right: -20px;
+    border-style: solid;
+    border-width: 30px 0px 30px 20px;
+    border-color: transparent transparent transparent
+      ${({ selected }) => (selected ? selectedColor : unSelectedColor)};
+    z-index: 1;
+  }
 
   :first-child {
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
 
-    &:after {
-      content: "";
-      position: absolute;
-      right: 0px;
-      border-top: 30px solid ${rootBackgroundColor};
-      border-bottom: 30px solid ${rootBackgroundColor};
-      border-left: 10px solid ${(props) => (props.selected ? selectedColor : unSelectedColor)};
+    &:before {
+      border-color: transparent;
     }
   }
 
@@ -31,46 +57,30 @@ const Styled = styled.div<{ selected: boolean | undefined }>`
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
 
-    &:before {
-      content: "";
-      position: absolute;
-      left: 0px;
-      border-top: 30px solid transparent;
-      border-bottom: 30px solid transparent;
-      border-left: 10px solid ${rootBackgroundColor};
-    }
-  }
-
-  :not(:first-child):not(:last-child) {
-    &:before {
-      content: "";
-      position: absolute;
-      left: 0px;
-      border-top: 30px solid transparent;
-      border-bottom: 30px solid transparent;
-      border-left: 10px solid ${rootBackgroundColor};
-    }
-
     &:after {
-      content: "";
-      position: absolute;
-      right: 0px;
-      border-top: 30px solid ${rootBackgroundColor};
-      border-bottom: 30px solid ${rootBackgroundColor};
-      border-left: 10px solid ${(props) => (props.selected ? selectedColor : unSelectedColor)};
+      border-color: transparent;
     }
   }
 
-  background: ${(props) => (props.selected ? selectedColor : unSelectedColor)};
+  :last-child {
+    margin-right: 0;
+  }
+
+  background: ${({ selected }) => (selected ? selectedColor : unSelectedColor)};
 `;
 
 interface Props {
   title: string;
   selected?: boolean;
+  parentBackground?: string | undefined;
 }
 
-const Step: React.FC<Props> = ({ title, selected }) => {
-  return <Styled selected={selected}>{title}</Styled>;
+const Step: React.FC<Props> = ({ title, selected, parentBackground }) => {
+  return (
+    <Styled parentBackground={parentBackground} selected={selected}>
+      {title}
+    </Styled>
+  );
 };
 
 export default Step;
